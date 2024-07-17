3c886051ec83a47a29bf3dbd5318e3d095343eed
Elisandro Santolin <elisandro.santolin@outlook.com>
2024-07-16 21:49:46 -0300

    Chor: added test file

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ TOKEN=EERRRR   ???????????????MTI1MjMxOTI1Nzk5MjIzNzE2Nw.????????GGjN6Z.Xr3zB68g
 CLIENT_ID=1252319257992237167
 GUILD_ID=798270637420183593
 JIRA_URL=https://armariosinteligentes.atlassian.net
-JIRA_USER_EMAIL=suporte2@armariosinteligentes.com.br
-JIRA_API_TOKEN=eretertrtrfrffgrgrgrgr??????/ATATT3xFfGF0oX6eNlCV3U_K2KgwAVwiypOS6aElQ6HhvZLoLgUkztCRWzFMecwmHalA39TrHWK88b4RzyOVxLFAsXvmzRZ3BGLHnJjsy3dgv8ZdgTIJegKMnXCjgOYnb8N3H_8p__nO3d6KHJjqo-1riQpp4W07h36UFt3HH6QDSCSON1sLDEM=C1092A77
+JIRA_USER_EMAIL=s//////uporte2@armariosinteligentes.com.br
+JIRA_API_TOKEN=eretertrtrfrffgrgrgrgr??????/ATATT3xFfGF0oX6eNlCV3U_K2???????????KgwAVwiypOS6/////aElQ6HhvZLoLgUkztCRWzFMecwmHalA39TrHWK88b4RzyOVxLFAsXvmzRZ3BGLHnJjsy3dgv8ZdgTIJegKMnXCjgOYnb8N3H_8p__nO3d6KHJjqo-1riQpp4W07h36UFt3HH6QDSCSON1sLDEM=C1092A77
 JIRA_PROJECT_KEY=RSUP
